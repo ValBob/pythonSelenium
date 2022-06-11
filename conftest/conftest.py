03cecd98ab4af3106d8832a78fef12c13071605f
Val Bob <val.bob@example.com>
2022-06-11 10:43:55 +0200

    to review later

--- conftest/conftest.py
+++ conftest/conftest.py
@@ -10,7 +10,7 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    browser= None
+    browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         browser = webdriver.Chrome()
